e9ceeaa005a9b71e440a3407fbeedc1f63851813
jesse.langat@student.moringaschool.com <jesse.langat@student.moringaschool.com>
2024-04-04 00:58:01 +0300

    Users/macbook/Documents/PHASE-1/loop/phase-0-intro-to-js-2-looping-code-along/.git/

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName){
+
+    let messageArray =[]
+  
+    for(let index = 0; index < namesArray.length; index++){
+      
+      let message = (`Thank you, ${namesArray[index]}, for the wonderful ${eventName} gift!`);
+  
+      messageArray.push(message);
+  
+    }
+  
+    return messageArray;
+  
+  }
+  
+  function countDown(number){
+  
+    let count = number;
+  
+    while(count >= 0){
+  
+      console.log(count);
+  
+      count--;
+  
+    }
+  
+  }
